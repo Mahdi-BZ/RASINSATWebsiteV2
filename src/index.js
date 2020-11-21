831b88ed0b4369ba55462eeace2723999f20943b
mahdi ben zinouba <mahdouch99@live.fr>
2020-11-21 17:36:04 +0100

    test google analytics 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import ReactGA from 'react-ga';
-ReactGA.initialize('233487014'); // add your tracking id here.
+ReactGA.initialize('UA-39719885-1'); // add your tracking id here.
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.querySelector("#root"));
